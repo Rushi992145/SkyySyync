087672f59a4cfe214e68cca89e37d3da3ff7f5eb
rushi992145 <rushikeshiray99@gmail.com>
2024-08-13 19:09:48 +0530

    http to https

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const API_URL = "http://api.weatherapi.com/v1/forecast.json?";
+const API_URL = "https://api.weatherapi.com/v1/forecast.json?";
 const API_KEY = "09303b02e0494feb982123006242307";
 
 function Card({ latitude, longitude, wcity }) {
